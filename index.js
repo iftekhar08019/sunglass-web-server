81a3a46ba20d93aee52100bed3298cd8d5f510c9
Md. Iftekharul Alam <iftekhar08019@gmail.com>
2021-11-19 22:49:21 +0600

    ok

--- index.js
+++ index.js
@@ -110,25 +110,7 @@ async function run() {
       res.json(result);
     });
 
-    app.put("/users/admin", verifyToken, async (req, res) => {
-      const user = req.body;
-      const requester = req.decodedEmail;
-      if (requester) {
-        const requesterAccount = await usersCollection.findOne({
-          email: requester,
-        });
-        if (requesterAccount.role === "admin") {
-          const filter = { email: user.email };
-          const updateDoc = { $set: { role: "admin" } };
-          const result = await usersCollection.updateOne(filter, updateDoc);
-          res.json(result);
-        }
-      } else {
-        res
-          .status(403)
-          .json({ message: "you do not have access to make admin" });
-      }
-    });
+    
 
     // DELETE API
     app.delete("/services/:id", async (req, res) => {
